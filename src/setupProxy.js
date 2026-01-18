218f86adcbd22dd461906c51acfa307f75b7cd34
Bong <bongithub02@gmail.com>
2026-01-19 01:01:43 +0900

    feat:remote api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api/inpaint",
     createProxyMiddleware({
-      target: "http://127.0.0.1:8090",
+      target: 'https://hugmebabe-suelgi-io-api.hf.space',
       changeOrigin: true,
       // 지금 너 상황에서는 프록시 단계에서 path가 '/'로 들어오고 있어서
       // 그냥 무조건 inpaint 엔드포인트로 바꿔버리는 게 제일 확실함
